61ae0adfd962243326a63dd9581189389b8f9296
sbp <1830109234@qq.com>
2022-05-13 10:53:46 +0800

    sbp-gitee commit6

--- src/main/java/com/sbp/git/GitTest1.java
+++ src/main/java/com/sbp/git/GitTest1.java
@@ -14,5 +14,6 @@ public class GitTest1 {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("GitHub test!");
+        System.out.println("GitEE test!");
     }
 }
