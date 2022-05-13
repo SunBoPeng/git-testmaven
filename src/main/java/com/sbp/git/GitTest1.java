f1ff99d723b4e4424a98f7acf322e5393ae6e26b
sbp <1830109234@qq.com>
2022-05-13 09:17:04 +0800

    sbp commit1

---
+++ src/main/java/com/sbp/git/GitTest1.java
@@ -0,0 +1,11 @@
+package com.sbp.git;
+
+/**
+ * @author 机械师小孙
+ * @version 1.0.1
+ */
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
